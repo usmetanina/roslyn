60d4c9941a81e98e3341b6b12c5e690f87b670f2
usmetanina <usmetanina96@mail.ru>
2018-04-17 21:39:55 +0500

    реализован подсчёт без именованных переменных (ввод только X и Y)

---
+++ roslyn/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+namespace roslyn
+{
+    class Calculator : System.MarshalByRefObject
+    {
+        public double Calc(string expression, Globals globals)
+        {
+            return CSharpScript.EvaluateAsync<double>(expression,
+                ScriptOptions.Default.WithImports("System.Math"), globals: globals).Result;
+        }
+    }
+}
---
+++ roslyn/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace roslyn
+{
+    [Serializable]
+    public class Globals
+    {
+        public int X;
+        public int Y;
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AppDomain domain = AppDomain.CreateDomain("test");
+            domain.Load(Assembly.GetExecutingAssembly().GetName());
+            var calc = (Calculator)domain.CreateInstanceAndUnwrap(
+                Assembly.GetExecutingAssembly().FullName, "roslyn.Calculator");
+
+            while (true)
+            {
+                Console.Write(">> ");
+                var input = Console.ReadLine();
+
+                if (input == "exit")
+                    break;
+
+                Console.WriteLine("Введите X");
+                var x = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите Y");
+                var y = Convert.ToInt32(Console.ReadLine());
+
+                var gl = new Globals { X = x, Y = y };
+                Console.WriteLine(calc.Calc(input, gl)); 
+            }
+        }
+    }
+}
